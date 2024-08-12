8525a65e64ef7438dabb177c401cb847730f1d25
Michail Kalogeras <89188677+MK-XXXVII@users.noreply.github.com>
2024-08-12 10:55:04 +0200

    tiny changes

--- prismicio-types.d.ts
+++ prismicio-types.d.ts
@@ -164,6 +164,17 @@ interface SettingsDocumentData {
    * - **Documentation**: https://prismic.io/docs/field#rich-text-title
    */
   siteTitle: prismic.TitleField;
+
+  /**
+   * Site Logo field in *Settings*
+   *
+   * - **Field Type**: Image
+   * - **Placeholder**: *None*
+   * - **API ID Path**: settings.siteLogo
+   * - **Tab**: Main
+   * - **Documentation**: https://prismic.io/docs/field#image
+   */
+  siteLogo: prismic.ImageField<never>;
 }
 
 /**
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,9 @@
 import "./globals.css";
 
 import { Inter } from "next/font/google";
-import { asText } from "@prismicio/client";
-import { PrismicText } from "@prismicio/react";
-import { PrismicNextLink, PrismicPreview } from "@prismicio/next";
-
-import { createClient, repositoryName } from "@/prismicio";
-import { Bounded } from "@/components/Bounded";
+import { PrismicPreview } from "@prismicio/next";
+import Header from "@/components/Header";
+import { repositoryName } from "@/prismicio";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -25,38 +22,5 @@ export default async function RootLayout({
         <PrismicPreview repositoryName={repositoryName} />
       </body>
     </html>
-  );
-}
-
-async function Header() {
-  const client = createClient();
-  const settings = await client.getSingle("settings");
-  const navigation = await client.getSingle("navigation");
-
-  return (
-    <Bounded as="header" yPadding="sm">
-      <div className="flex flex-wrap items-baseline justify-between gap-x-6 gap-y-3 leading-none">
-        <PrismicNextLink
-          href="/"
-          className="text-xl font-semibold tracking-tight"
-        >
-          <PrismicText field={settings.data.siteTitle} />
-        </PrismicNextLink>
-        <nav>
-          <ul className="flex flex-wrap gap-6 md:gap-10">
-            {navigation.data?.links.map((item) => (
-              <li
-                key={asText(item.label)}
-                className="font-semibold tracking-tight text-slate-800"
-              >
-                <PrismicNextLink field={item.link}>
-                  <PrismicText field={item.label} />
-                </PrismicNextLink>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </div>
-    </Bounded>
   );
 }
